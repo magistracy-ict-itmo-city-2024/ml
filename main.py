35fb054caea5cad5241ee7ab3b1ee039333c805a
daryabaldina1702 <ddbaldina@mail.ru>
2024-10-11 10:43:01 +0300

    main functional

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from fastapi import FastAPI
+import os
+import environ
 
 import ml
 from dto import IssueDto, ResponseDto
 
 app = FastAPI()
 BASE_URL = "https://https://city24.civiltechgroup.ru/"
-
+env = environ.Env()
+environ.Env.read_env()
 
 def generate_file_url(issue: IssueDto):
     return f"{BASE_URL}api/v0/issues/{issue.id}/downloadFile"
 
+ 
 
 @app.post("/")
 async def analyze_recommendation(
         issue: IssueDto
 ):
+    token = env('HUGGINGFACE_TOKEN')
     file_url = generate_file_url(issue)
-    ml.some_function(issue, file_url)
-    return ResponseDto(prediction=0.5)
+    #иницилизация модели
+    model = ml.init_model(token, issue.categoryId)
+    #получение распознанных объектов
+    labels = ml.detection(model, file_url)
+    #получение коэффициента доверия
+    coef_of_trust = ml.get_response(token, issue.description, labels, issue.categoryId)
+    return ResponseDto(prediction=coef_of_trust)
--- ml.py
+++ ml.py
@@ -1,8 +1,85 @@
-from dto import IssueDto
+# from dto import IssueDto
+from huggingface_hub import InferenceClient
+import json
+from googletrans import Translator
+import random
 
 
-def some_function(
-        issue_dto: IssueDto,
-        file_url: str
-):
-    print("Hello, world!")
+
+def translate_text(text, target_language='en'):
+    translator = Translator()
+    translation = translator.translate(text, dest=target_language)
+    return translation.text
+
+
+def get_response(token, inputs, labels, category_id):
+        #обработка языка
+        description = translate_text(inputs)
+
+        #допустим до этого пришел какой-то коэфф доверия (его нет в MVP)
+        coeff = round(random.uniform(0.9, 1), 2)
+
+        #категории по распознанным объектам
+        categories = {
+               "trash" : ['paper', 'plastic', 'cardboard', 'glass', 'metal'],
+               "accident": ['car', 'motorcycle', 'bus', 'train', 'truck', 'traffic light', 'stop sign', 'person'],
+               "bad weather": ['umbrella', 'person'],
+               "renovation work": ['truck', 'traffic light', 'stop sign', 'person', 'fire hydrant']
+        }
+
+        #названия категорий по id
+        categories_names = {
+               1: "situation with trash",
+               2: "bad weather",
+               3: "danger",
+               4: "city problems"
+        }
+
+        #подготовка labels
+        candidate_labels = []
+        for i in categories:
+                for j in labels:
+                        if j in categories[i]:
+                                candidate_labels.append(i)
+        candidate_labels = list(set(candidate_labels))
+
+        if not candidate_labels:
+                candidate_labels.append(categories_names[category_id])
+
+        #обработка результатов модели
+        client_nlp = InferenceClient(token=token)
+        params = {"candidate_labels": candidate_labels}
+        response = client_nlp.post(json={"inputs": inputs, "parameters": params}, model="typeform/distilbert-base-uncased-mnli")
+        response = response.decode()
+        response = json.loads(response)
+        
+        #расчет коэффициента доверия
+        pred_label, pred_score = response['labels'][0], response['scores'][0]
+
+        if (pred_label == "trash" or pred_label == "situation with trash") and category_id == 1 and pred_score > 0.7:
+                return coeff  
+        elif  (pred_label == "accident" or pred_label == "danger") and category_id == 3 and pred_score > 0.7:
+                return coeff
+        elif  pred_label == "bad weather" and category_id == 2 and pred_score > 0.7:
+                return coeff
+        elif  (pred_label == "renovation work" or pred_label == "city problems") and category_id == 4 and pred_score > 0.7:
+                return coeff
+        else:
+                return coeff - 0.1
+    
+
+def init_model(token, category_id):  
+        if category_id == 1:
+                client = InferenceClient(model = "pvallej3/garbage_classifier", token=token)
+        else:
+                client = InferenceClient(model = "facebook/detr-resnet-50", token=token)
+        
+        return client
+
+def detection(model, pic):
+        response = model.object_detection(pic)
+        labels = [] 
+        for i in response:
+              if i['score'] > 0.7:
+                     labels.append(i)
+        return list(set(labels))
